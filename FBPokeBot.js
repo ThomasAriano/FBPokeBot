277205dc93fd33c12a8f418a6102493abd0035be
Thomas <thomas.ariao@gmail.com>
2017-04-18 12:03:37 -0400

    Updates

--- FBPokeBot.js
+++ FBPokeBot.js
@@ -1,6 +1,7 @@
     var pokeCount = 0;
     var timer = 0;
     var runtimeSec = 0;
+    var canRun = false;
 
     function poke()
     {
@@ -8,11 +9,13 @@
             return (goodLink.innerHTML.contains("Poke Back"));
         });
         for (var i = 0 ; i != pokeLinks.length; i++) {
-            pokeLinks[i].click();
-            pokeCount++;
+            if(canRun) {
+                pokeLinks[i].click();
+                pokeCount++;
+            }
         }
 
-        var timeOutMS = Math.floor(Math.random() * (500000 - 40000)) + 40000;
+        var timeOutMS = Math.floor(Math.random() * (500000 - 100000)) + 100000;
         console.log("Next poke: "  + moment().add(Math.floor(timeOutMS/1000), 'seconds').format('LTS'));
         timer = Math.floor(timeOutMS/1000);
         setTimeout(countdownTimer,1000);
@@ -20,19 +23,29 @@
 
     function updateDisplay()
     {
+        if(document.getElementById("id_cb_enable").checked) {
+            canRun = true;
+        } else {
+            canRun = false;
+        }
+
         var pokeHeader = Array.prototype.slice.call(document.getElementsByTagName("a")).filter(function (goodLink) {
             return (goodLink.innerHTML.contains("Pokes"));
         });
-        pokeHeader[1].innerHTML = "<a href='/pokes/'>Pokes: " + pokeCount + ", Next Poke: " + timer + "s</a>";
+        pokeHeader[1].innerHTML = "<a href='https://github.com/ThomasAriano/FBPokeBot'>Pokes: " + pokeCount + ", Next Poke: " + timer + "s</a>";
 
         document.getElementById("runtime_div").innerHTML = "Runtime: " + runtimeSec;
     }
 
     function countdownTimer()
     {
-        runtimeSec++;
+        if(canRun) {
+            runtimeSec++;
+        }
         if(timer !== 0) {
-            timer--;
+            if(canRun) {
+                timer--;
+            }
             updateDisplay();
             setTimeout(countdownTimer,1000);
         } else {
@@ -48,7 +61,7 @@
         runtimeDiv.style.position = "absolute";
         runtimeDiv.style.zIndex = "999";
         runtimeDiv.style.left = "15px";
-        runtimeDiv.style.top = "43px";
+        runtimeDiv.style.top = "40px";
         runtimeDiv.style.width = "100px";
         runtimeDiv.style.textAlign = "center";
         runtimeDiv.style.border = "1px #5555ff solid";
@@ -63,7 +76,7 @@
         infoDiv.style.position = "absolute";
         infoDiv.style.zIndex = "999";
         infoDiv.style.left = "115px";
-        infoDiv.style.top = "43px";
+        infoDiv.style.top = "40px";
         infoDiv.style.width = "100px";
         infoDiv.style.textAlign = "center";
         infoDiv.style.border = "1px #5555ff solid";
@@ -76,8 +89,27 @@
             return (test.outerHTML.contains("Poke your friends"));
         });
         textBox[0].outerHTML = "<input type='text' class='inputtext' maxlength='100' aria-label='Search'" +
-        "placeholder='Find more victims' autocomplete='off' aria-autocomplete='list' aria-expanded='false'"+
-        "aria-controls='typeahead_list_u_0_b' aria-haspopup='true' role='combobox' spellcheck='false'>";
+            "placeholder='Find more victims' autocomplete='off' aria-autocomplete='list' aria-expanded='false'"+
+            "aria-controls='typeahead_list_u_0_b' aria-haspopup='true' role='combobox' spellcheck='false'>";
+
+        var checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.name = "cb_Start";
+        checkBox.value = "Enabled";
+        checkBox.id = "id_cb_enable";
+        checkBox.style.position = "absolute";
+        checkBox.style.left = "220px";
+        checkBox.style.top = "38px";
+        document.getElementsByClassName("uiHeader _5i1b uiHeaderPage")[0].appendChild(checkBox);
+
+        var cbLabel = document.createElement('label');
+        cbLabel.htmlFor= "id_cb_enable";
+        cbLabel.style.position = "absolute";
+        cbLabel.style.left = "240px";
+        cbLabel.style.top = "40px";
+        cbLabel.appendChild(document.createTextNode('Enable'));
+        document.getElementsByClassName("uiHeader _5i1b uiHeaderPage")[0].appendChild(cbLabel);
+
     }
 
     paintGUI();
